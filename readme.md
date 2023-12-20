2bf3be0ddb89bcef24aa5f5e55290e8fea2785bb
RokasV <vezikauskas.r@gmail.com>
2023-12-20 11:45:21 +0200

    get all

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Steps
+
+1. `npm i mysql2`
+2. `https://www.npmjs.com/package/mysql2#using-promise-wrapper`
+3. `const mysql = require('mysql2/promise');`
+4. `const connection = mysql.createConnection(
+  {host:'localhost', user: 'root', database: 'test'}
+);`
--- src/server.js
+++ src/server.js
@@ -1,14 +1,58 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
 const app = express();
 
 const PORT = 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// Middleware
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+//  GET - /api/posts -grazins visus postus
+app.get('/api/posts', async (req, res) => {
+  try {
+    // prisijungti prie DB
+    const conn = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'type19_db',
+    });
+    // atlikti veksma
+    const sql = 'SELECT * FROM `posts`';
+    const [rows, fields] = await conn.query(sql);
+    console.log('rows ===', rows);
+    // grazinti duomenis
+    res.json(rows);
+    // atsijungti nuo DB
+  } catch (error) {
+    console.log('error ===', error);
+    console.log(' klaida get posts');
+    res.status(500).json({
+      msg: 'Something went wrong',
+    });
+  } finally {
+    if (conn) conn.end();
+  }
 });
 
+// Get - /api/posts/5 grazins 5 posta
+app.get('/api/posts/:pId', (req, res) => {
+  try {
+    res.json('get single post');
+  } catch (error) {
+    console.log('error ===', error);
+    console.log(' klaida get posts');
+    res.status(500).json({
+      msg: 'Something went wrong',
+    });
+  }
+});
 app.listen(PORT, () => {
   `Server running on http://localhost:${PORT}`;
 });
